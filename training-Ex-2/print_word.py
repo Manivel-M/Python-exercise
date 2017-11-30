7be36050e8f419fff987cf0d3bd68baee4beda6d
Manivel-M <33547843+Manivel-M@users.noreply.github.com>
2017-11-30 17:01:27 +0530

    Update print_word.py

--- training-Ex-2/print_word.py
+++ training-Ex-2/print_word.py
@@ -1,3 +1,5 @@
+"""Print the letters from the word - "gnu-linux is rule the world now'"""
+
 user='gnu-linux is rule the world now'
 for letter in user:
 	if '-' not in letter and ' ' not in letter:
