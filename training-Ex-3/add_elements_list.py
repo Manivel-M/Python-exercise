6983f91b17ff2e7a14d93ce807c27b73cd26d529
Manivel-M <33547843+Manivel-M@users.noreply.github.com>
2017-12-27 14:22:34 +0530

    Removed 0 mentioned in length of the list

--- training-Ex-3/add_elements_list.py
+++ training-Ex-3/add_elements_list.py
@@ -4,7 +4,7 @@ user = []
 choice = 'y'
 while choice == 'y':
 	length = int(raw_input("Enter the length of the list :"))
-	for count in range(0,length):
+	for count in range(length):
 		user.append(raw_input("Enter the input :"))
 	print user
 	choice = raw_input("Do u need to add elements in list :")
