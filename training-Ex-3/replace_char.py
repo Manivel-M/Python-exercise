77f15eeb5eddbc1370cf6e39ade5dc4989be0614
Manivel-M <33547843+Manivel-M@users.noreply.github.com>
2018-01-09 14:24:38 +0530

    Updated replace code
    Changed list conversion as from for loop to list()

--- training-Ex-3/replace_char.py
+++ training-Ex-3/replace_char.py
@@ -1,12 +1,9 @@
 """Write the program to replace the odd position string with X on any strings."""
 
 user_input = raw_input("Enter the string :")
-user_store = []
+user_list = list(user_input)
 for count in range(0,len(user_input)):
-	user_store.append(user_input[count])
 	if (count % 2) == 0:
-		user_store[count] = 'X'
-	
-user_store = ''.join(user_store)	
-print user_store
+		user_list[count] = 'X'	
+print (''.join(user_list))
 
