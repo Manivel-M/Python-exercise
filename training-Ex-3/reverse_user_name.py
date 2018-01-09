c23e1dbbcf6003ef2b6f7a7579b5fece0360d1a7
Manivel-M <33547843+Manivel-M@users.noreply.github.com>
2018-01-09 14:13:52 +0530

    Updated code to reverse the input without for loop

--- training-Ex-3/reverse_user_name.py
+++ training-Ex-3/reverse_user_name.py
@@ -1,9 +1,5 @@
 """Write the program to get the name of user and reverse it"""
 
 user_name = raw_input("Enter the name :")
-user_store=[]
-for i in range(len(user_name),0,-1):
-	user_store.append(user_name[i-1])
-user_store=''.join(user_store)
-print user_store
+print (''.join(reversed(a))
 	
