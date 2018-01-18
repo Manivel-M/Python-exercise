127654ac2cd5faa25e0ce11cf6838612cc1b7b08
Manivel-M <33547843+Manivel-M@users.noreply.github.com>
2018-01-18 19:04:15 +0530

    Delete vowels_and_consonant.py

--- training-Ex-2/vowels_and_consonant.py
+++
@@ -1,9 +0,0 @@
-vowels=['a','e','i','o','u']
-user_input=raw_input("Enter the string :")
-for count_1 in user_input:
-	if count_1 in vowels:
-		print ('vowel',count_1)
-	else:
-		print('consonant',count_1)
-
-	
