21a688cb5e67d6e65627c737631ae3ce212f955e
Manivel-M <manivel.manickam@vvdntech.in>
2018-02-08 18:31:03 +0530

    exercise 5

---
+++ training-ex-5/count_no.of_lines_in_no.of_file.py
@@ -0,0 +1,27 @@
+"""Write a program to count no of lines in n no of text files and Plot it as a bar graph as File vs Lines."""
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+user_input=raw_input("Enter the file name :")
+file_co=user_input.split()
+no_line_file = 0  #Number of lines in a file
+no_file = len(file_co)  #Number of files
+for count,name in enumerate(file_co):
+	file_op=open(name,"r")
+	no_line_file += len(file_op.readlines())
+	file_op.close()	
+print no_line_file
+print no_file
+ 
+objects = ('Number.of.lines','Number.of.files')
+y_pos = np.arange(len(objects))
+performance = [no_line_file,no_file]
+ 
+plt.bar(y_pos, performance, align='center', alpha=0.5)
+plt.xticks(y_pos, objects)
+plt.ylabel('Count')
+plt.title('Comparison between number of files and number of lines in files ')
+ 
+plt.show()
+
