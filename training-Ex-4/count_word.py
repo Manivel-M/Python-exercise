88a01828a8e5d1e510e9ee33279f2cb30a53a589
Manivel-M <33547843+Manivel-M@users.noreply.github.com>
2019-05-03 15:13:49 +0530

    Modified comment in GUI

--- training-Ex-4/count_word.py
+++ training-Ex-4/count_word.py
@@ -1,7 +1,7 @@
 
 """ Return the number of times that the string "code" appears anywhere in the given string, except
 we will accept any letter for the d, so "cope" and "cooe" count.
-Hount_code('aaacodebbb') =1
+count_code('aaacodebbb') =1
 count_code('codexxcode') =2
 count_code('cozexxcope') =2 """
 
